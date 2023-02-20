1df2793557dd36a25bfc5d9b7455ac2ba90e619d
Vignesh K S <vigneshk@cloudthat.com>
2023-02-20 20:28:30 +0530

    first4

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "4.53.1"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
-  }
+  # required_providers {
+  #   google = {
+  #     source  = "hashicorp/google"
+  #     version = "4.53.1"
+  #   }
+  #   random = {
+  #     source  = "hashicorp/random"
+  #     version = "3.0.1"
+  #   }
+  # }
   required_version = ">= 1.1.0"
 
   cloud {
--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "google_compute_network" "vpc_network" {
-  name                    = "new-networks"
+  name                    = "new-network"
   auto_create_subnetworks = false
   
 }
